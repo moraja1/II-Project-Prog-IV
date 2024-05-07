16be499f174364c40d85b995a8d14d694a2edceb
Jason Mora <jeisonmora0115@gmail.com>
2024-05-07 11:01:29 -0600

    Preparing repo for TDD development

--- invoicesSystem/src/test/resources/data.sql
+++ invoicesSystem/src/test/resources/data.sql
@@ -1,23 +1,3 @@
-INSERT INTO `projectii`.`user`
-(`natural_id`,
- `password`,
- `name`,
- `last_name`,
- `mobile`,
- `email`,
- `enabled`,
- `type`,
- `role`)
-VALUES ('101110111',
-        'admin',
-        'Jaison',
-        'Mora',
-        '88888888',
-        'jaison@admin.com',
-        1,
-        'Physical',
-        'Admin');
-
 INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
                                 `role`)
 VALUES ('202220222', 'oW0~kAA91qF', 'Dana', 'Lisett', '9636350406', 'dlisett0@intel.com', '0', 'Physical', 'Supplier');
@@ -33,46 +13,3 @@ INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `
                                 `role`)
 VALUES ('505550555', 'oT6/9>cqFo', 'Zsazsa', 'Galiford', '4426626874', 'zgaliford3@ebay.com', '0', 'Juridical',
         'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('606660666', 'qJ8@.7@r!8c&v', 'Keir', 'Pleasaunce', '1943421528', 'kpleasaunce4@examiner.com', '1', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('707770777', 'zJ0~*=\'Qa', 'Ginger', 'Mocher', '7367040293', 'gmocher5@usatoday.com', '1', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('808880888', 'aK4\'|eVJz=ky(pX', 'Adella', 'Giovanitti', '8971543494', 'agiovanitti6@storify.com', '1',
-        'Physical', 'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('909990999', 'rA3#6CA%V0eb', 'Rollie', 'Mainston', '5901274861', 'rmainston7@webmd.com', '1', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('102220222', 'lY3`Y0`io\'iv$po', 'Lori', 'Callicott', '6247199136', 'lcallicott8@myspace.com', '1', 'Juridical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('201110111', 'qP1((~6OTN/', 'Flossie', 'Scurlock', '5558281678', 'fscurlock9@dell.com', '1', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('301110111', 'lH7?7S@`C|eil', 'May', 'McKinnon', '8952110276', 'mmckinnona@twitpic.com', '0', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('401110111', 'aH4\zl1?3eH|', 'Viole', 'Massey', '2197397979', 'vmasseyb@boston.com', '0', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('501110111', 'dY7,)@Fw1pB)2', 'King', 'Jirek', '9128497348', 'kjirekc@tinypic.com', '0', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('601110111', 'iX5@0*y_fEM0=', 'Constanta', 'Lagne', '6886341718', 'clagned@ocn.ne.jp', '1', 'Physical',
-        'Supplier');
-INSERT INTO `projectii`.`user` (`natural_id`, `password`, `name`, `last_name`, `mobile`, `email`, `enabled`, `type`,
-                                `role`)
-VALUES ('701110111', 'nR0|SM&H>7', 'Kevan', 'Speedy', '3588880009', 'kspeedye@wp.com', '0', 'Juridical', 'Supplier');
